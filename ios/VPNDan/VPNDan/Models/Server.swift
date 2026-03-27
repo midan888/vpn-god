c3d7c93ac9fce5d77ca1c4bf14cb5cb9e8a94a0f
Mika Danielin <midan888@gmail.com>
2026-03-27 17:09:07 +0300

    server latency

--- ios/VPNDan/VPNDan/Models/Server.swift
+++ ios/VPNDan/VPNDan/Models/Server.swift
@@ -13,4 +13,23 @@ struct Server: Codable, Identifiable {
         case pingPort = "ping_port"
         case isActive = "is_active"
     }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        country = try container.decode(String.self, forKey: .country)
+        host = try container.decode(String.self, forKey: .host)
+        pingPort = try container.decodeIfPresent(Int.self, forKey: .pingPort) ?? 8080
+        isActive = try container.decode(Bool.self, forKey: .isActive)
+    }
+
+    init(id: UUID, name: String, country: String, host: String, pingPort: Int = 8080, isActive: Bool) {
+        self.id = id
+        self.name = name
+        self.country = country
+        self.host = host
+        self.pingPort = pingPort
+        self.isActive = isActive
+    }
 }
